a3d9b6cf811a57878af39c68119c127535fbcd9b
RobJones05 <roberttamlinjones@gmail.com>
2026-04-08 08:54:32 +0200

    created array to hold soccer teams

---
+++ topsoccerteams/app/src/main/java/com/example/top_soccer_teams/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.top_soccer_teams
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    val teams = arrayOf<String>("Mamelodi Sundowns FC", "Orlando Pirates", "Bidvest Wits",
+        "Stellenbosch FC", "Sek United")
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
